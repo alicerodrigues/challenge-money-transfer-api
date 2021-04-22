09c82833fc02ce6754d8564bc796ed886da72e61
Alice Rodrigues <alice_rodriguess@hotmail.com>
2021-04-22 13:03:24 -0300

    Insert min for amount

--- src/main/java/challengemoneytransferapi/model/dto/TransferDTO.java
+++ src/main/java/challengemoneytransferapi/model/dto/TransferDTO.java
@@ -2,6 +2,7 @@ package challengemoneytransferapi.model.dto;
 
 import java.math.BigDecimal;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
@@ -17,6 +18,7 @@ public class TransferDTO {
 	private Long accountFromId;
 	@NotNull(message = "AccountToId is mandatory")
 	private Long accountToId;
+	@Min(value = 1, message = "Amount should not be less than 1")
 	@NotNull(message = "Amount is mandatory")
 	private BigDecimal amount;
 
